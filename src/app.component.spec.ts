d71c5d64562e0a9b609c50c607c893e017a89bbc
Christos Koutsiaris <koutsiaris@gmail.com>
2016-09-12 16:50:56 +0100

    fix tests

--- src/app.component.spec.ts
+++ src/app.component.spec.ts
@@ -1,11 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+import { InputForm } from './form.component'
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent] });
+    TestBed.configureTestingModule({ declarations: [AppComponent, InputForm], imports: [FormsModule] });
   });
+
   it('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
   });
+
+  it(`should have as title 'Angular With Webpack'`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Angular With Webpack');
+  }));
+
 });
--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { TestInput } from './test-input';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Angular With Webpack';
+}
